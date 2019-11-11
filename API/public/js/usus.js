8a3602303b27ec8376502c70e18f0511268be516
jddr27 <jddeleonr.inf2013@gmail.com>
2019-11-11 06:50:37 -0600

    asd

---
+++ API/public/js/usus.js
@@ -0,0 +1,37 @@
+/*var socket = io.connect('http://130.211.112.112:3001', { 'forceNew': true });
+
+socket.on('messages', function(data) {
+  console.log(data);
+  render(data);
+})
+
+function render (data) {
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {
+  var message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  socket.emit('new-message', message);
+  return false;
+}
+*/
+var news = io.connect('http://130.211.112.112:3001/news');
+  
+  news.on('connect', function () {
+    /*chat.emit('hi!');*/
+  });
+  
+  news.on('news', function () {
+    news.emit('woot');
+  });
