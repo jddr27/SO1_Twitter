2fbb14d739d76e3ebbddf189652c6b0aa71d0f0f
jddr27 <jddeleonr.inf2013@gmail.com>
2019-11-13 19:36:00 -0600

    asd

--- API/public/js/tweets.js
+++
@@ -1,7 +0,0 @@
-var chat = io.connect('http://130.211.112.112:3001/chat', { 'forceNew': true });
-  
-  chat.on('hi', function (data) {
-    /*chat.emit('hi!');*/
-    document.body.innerHTML = '';
-    document.write(data);
-  });
